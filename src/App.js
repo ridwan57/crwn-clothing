4e7c1baf1caf883eeb53cd4f998d67b2ab546f06
ridwan57 <ridwananik57@gmail.com>
2020-08-09 23:06:27 +0600

    6.4

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
-const tempPage = () => (
-  <div>
-    <h1> Veve</h1>
-  </div>
-);
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
-    <div >
+    <div>
       <BrowserRouter>
+
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
         </Switch>
       </BrowserRouter>
-
-
     </div>
   );
-
 }
 
 export default App;
